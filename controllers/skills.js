05bfd04352da2e0eae2b1e5fe31cc5766b87a425
Jkokol2000 <112048803+Jkokol2000@users.noreply.github.com>
2023-02-01 11:16:13 -0800

    Added Partials for the header and footer for all pages.

--- controllers/skills.js
+++ controllers/skills.js
@@ -7,12 +7,14 @@ const skills = require('../models/skills');
 
 function index(req, res) {
     res.render('dev-skills/index', {
-        skills: skills.getAll()
+        skills: skills.getAll(),
+        title: "Main Skill Page"
     });
 }
 
 function show(req, res) {
     res.render('dev-skills/show', {
-        skills: skills.getOne(req.params.id)
+        skills: skills.getOne(req.params.id),
+        title: 'Current Skill'
     });
 }
