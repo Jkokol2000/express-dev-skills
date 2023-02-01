adaa300275191f0b89ea8fd07da7c2d7337110bd
Jkokol2000 <112048803+Jkokol2000@users.noreply.github.com>
2023-02-01 09:57:10 -0800

    Made default page the Skills Page

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', skillsRouter);
 app.use('/skills', skillsRouter);
 
 // catch 404 and forward to error handler
